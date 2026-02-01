db7e08cb561b2c649f6363422da85dab6b74f107
Soman <somandhir@gmail.com>
2026-02-02 00:17:13 +0530

    update for prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,20 +11,26 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+const origin =
+  process.env.NODE_ENV === "production"
+    ? false // Same-origin is usually fine when serving dist
+    : "http://localhost:5173";
+app.use(cors({ origin, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
